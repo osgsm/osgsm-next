9553ed07b7c6c989bd2f1c59a5f5f747461b89d6
osgsm <50955473+osgsm@users.noreply.github.com>
2026-02-26 15:16:55 +0900

    Improve copy button style

--- src/components/mdx/copy-button.tsx
+++ src/components/mdx/copy-button.tsx
@@ -21,13 +21,13 @@ export function CopyButton({ getCode }: { getCode: () => string }) {
       onClick={handleCopy}
       className={twMerge(
         'rehype-pretty-copy',
-        'absolute top-2 right-2 grid cursor-pointer place-items-center border border-border bg-iris-2 p-1 pl-2 font-pixel-square leading-none font-bold',
+        'absolute top-2 right-2 grid cursor-pointer place-items-center border border-border bg-iris-2 py-1 pr-1.5 pl-2 font-pixel-square leading-none font-bold',
         '*:col-span-full *:row-span-full'
       )}
     >
       <span
         className={twMerge(
-          '-translate-px text-sm text-iris-11 transition-all duration-200 ease-out',
+          '-translate-px text-sm text-iris-10 transition-all duration-200 ease-out',
 
           copied ? 'invisible opacity-0' : 'visible opacity-100'
         )}
@@ -36,7 +36,7 @@ export function CopyButton({ getCode }: { getCode: () => string }) {
       </span>
       <span
         className={twMerge(
-          'text-teal-9',
+          'text-teal-8',
           'transition-all duration-200 ease-out',
 
           copied ? 'visible opacity-100' : 'invisible opacity-0'
